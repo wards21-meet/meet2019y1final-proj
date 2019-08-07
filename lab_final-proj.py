399e4386d2fea74cbe16ace1179269f5296aa685
wards21-meet <wards21@meet.mit.edu>
2019-08-07 05:33:57 -0400

    Stopping here

--- lab_final-proj.py
+++ lab_final-proj.py
@@ -9,7 +9,7 @@ stamp_list = []
 food_stamps = []
 food_pos = []
 
-#turtle.bgpic('jungle(1).gif')
+turtle.bgpic('rocki.gif')
 SIZE_X=1100
 SIZE_Y=1100
 turtle.setup(SIZE_X, SIZE_Y)
@@ -30,18 +30,19 @@ def new_stamp():
 def move(x,y):
     while maze.xcor() != x:
         if maze.xcor()<x:
-            maze.goto((maze.xcor())+10,maze.ycor())
+            maze.goto((maze.xcor())+50,maze.ycor())
             new_stamp()
         elif maze.xcor()>x:
-            maze.goto((maze.xcor())-10,maze.ycor())
+            maze.goto((maze.xcor())-50,maze.ycor())
             new_stamp()
     while maze.ycor() != y:
         if maze.ycor()<y:
-            maze.goto((maze.xcor()),maze.ycor()+10)
+            maze.goto((maze.xcor()),maze.ycor()+50)
             new_stamp()
         elif maze.ycor()>y:
-            maze.goto((maze.xcor()),maze.ycor()-10)
+            maze.goto((maze.xcor()),maze.ycor()-50)
             new_stamp()
+            
 def drawmaze():
     maze.penup()
     maze.goto(-500,100)
@@ -106,7 +107,29 @@ def drawmaze():
 
 drawmaze()
 turtle.showturtle()
+food = turtle.clone()
+turtle.register_shape('chicken.gif')
+food.shape('chicken.gif')
 
+turtle.hideturtle()
+food.penup()
+
+turtle.showturtle()
+
+
+food_stamps=[]
+food_pos=[]
+
+
+
+for this_food_pos in food_pos:
+    food.goto(this_food_pos)
+    food_stamp = food.stamp()
+    food_stamps.append(food_stamp)
+
+food.hideturtle()
+
+#this one
 def make_food():
     min_x=-int(1000/2/50)+1
     max_x=int(1000/2/50)-1
@@ -115,10 +138,12 @@ def make_food():
 
     food_x = random.randint(min_x,max_x)*50
     food_y = random.randint(min_y,max_y)*50
-    food.goto(food_x,food_y)
-    food_pos.append(food.pos())
-    food_stamps.append(food.stamp())
-
+    food_xy=(food_x,food_y)
+    if food_xy not in pos_list:           
+        food.goto(food_x,food_y)
+        food_pos.append(food.pos())
+        food_stamps.append(food.stamp())
+    print('i added one food')
 
 
 
@@ -127,42 +152,38 @@ scar = turtle.clone()
 turtle.register_shape("hunter.gif")
 scar.shape("hunter.gif")
 
-for this_food_pos in food_pos:
-    food.goto(this_food_pos)
-    food_stamp = food.stamp()
-    food_stamps.append(food_stamp)
-
-food = turtle.Turtle()
-turtle.hideturtle()
-food.penup()
-turtle.register_shape('chicken.gif')
-food.shape('chicken.gif')
-turtle.showturtle()
 
 
-turtle.tracer
+
+
+turtle.tracer()
+
 turtle.penup()
 #makes the turtle move more smoothly.
 simba = turtle.clone()
 
 
-turtle.register_shape("simba.gif")
-simba.shape("simba.gif")
+turtle.register_shape("simba2.gif")
+simba.shape("simba2.gif")
 turtle.hideturtle()
 simba.goto(400,-350)
 simba.direction = 'Up'
 
 
-
+score=0
 
 #that will make simba move using the arrows:
 def move_simba():
+    #global food_stamps
     my_pos = simba.pos()
     x_pos = my_pos[0]
     y_pos = my_pos[1]
 
+    if simba.pos() in pos_list:
+        print("you touched the wall")
+        simba.goto(450,-450)
 #if you press the up arrow simba will move forward
-    if simba.direction == 'Up' :   
+    elif simba.direction == 'Up' :   
         simba.goto(x_pos , y_pos +50)
         print('you moved up')
 
@@ -178,16 +199,31 @@ def move_simba():
     elif simba.direction == 'Left' :
         simba.goto(x_pos - 50,y_pos)
         print('you moved left')
-        
+
+
+    global score
+
+       
     if simba.pos() in food_pos:
         food_index=food_pos.index(simba.pos())
         food.clearstamp(food_stamps[food_index])
         food_pos.pop(food_index)
         food_stamps.pop(food_index)
         print("You have eaten the food!")
+        score=score+1
+        turtle.undo()
+        turtle.penup()
+        turtle.hideturtle()
+        turtle.goto(-398,200)
+        turtle.pendown()
+        turtle.write(score, False,align="left", font=("arial",20,"normal"))
+
+
+        
+#this one
+    if len(food_stamps) <= 1:
+        make_food()
     
-    #if len(food_stamps) <= 1:
-    #make_food()
     
 def up():
     simba.direction = 'Up'
@@ -211,23 +247,11 @@ def left() :
     move_simba()
 turtle.onkeypress(left,'Left')
 
-
-
 turtle.listen()
 
-for this_food_pos in food_pos:
-    food.goto(this_food_pos)
-    food_stamp = food.stamp()
-    food_stamps.append(food_stamp)
 
-
-
-
-
-
-
-
-simba.speed(-2)        
+simba.speed(-2)
+        
 
         
 
@@ -239,7 +263,7 @@ y_Pos1=scar.pos()[1]
 
 while 1==1:
     scar.penup()
-    scar.speed(3)
+    scar.speed(5)
     scar.goto((random.randint(-500,500)),(random.randint(-500,500)))
 
 
@@ -256,7 +280,7 @@ while 1==1:
 
 
 
-
+print(pos_List)
 move_simba()
 
 turtle.mainloop()
